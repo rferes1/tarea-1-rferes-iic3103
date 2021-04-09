a6204b82cfd2e568f885e10c363e168e24c45103
rferes1 <rferes@uc.cl>
2021-04-09 13:05:45 -0400

    respaldo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,31 @@ require 'net/http'
 
 class PagesController < ApplicationController
 	def home
-		@response = HTTParty.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes')
-		@codigo = @response.parsed_response
+		@episodes_json = HTTParty.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes').parsed_response
+		@bb_arr = []
+		@bcs_arr = []
+		@episodes_json.each do |episode|
+			if episode['series'] == 'Breaking Bad'
+				@bb_arr.push(episode['season'])
+			elsif episode['series'] == "Better Call Saul"
+				@bcs_arr.push(episode['season'])
+			end
+		end
+		@bb_arr = @bb_arr.uniq
+		@bcs_arr = @bcs_arr.uniq
+
+		@colors = [
+			['rgba(53, 92, 125, 1)','rgba(53, 92, 125, 0.6)', 'rgba(53, 92, 125, 0.3)'],
+		    ['rgba(163, 111, 72, 1)','rgba(163, 111, 72, 0.6)','rgba(163, 111, 72, 0.3)'],
+		    ['rgba(201, 107, 69, 1)','rgba(201, 107, 69, 0.6)','rgba(201, 107, 69, 0.3)'],
+		    ['rgba(182, 151, 37, 1)','rgba(182, 151, 37, 0.6)','rgba(182, 151, 37, 0.3)'],
+		    ['rgba(32, 48, 106, 1)','rgba(32, 48, 106, 0.6)','rgba(32, 48, 106, 0.3)']]
+	end
+
+	def show_season
+		@season_name = params[:season_name]
+		@season_id = params[:sid]
+		@season_json = HTTParty.get("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=#{@season_name}").parsed_response
 		@colors = [
 			['rgba(53, 92, 125, 1)','rgba(53, 92, 125, 0.6)', 'rgba(53, 92, 125, 0.3)'],
 		    ['rgba(163, 111, 72, 1)','rgba(163, 111, 72, 0.6)','rgba(163, 111, 72, 0.3)'],
@@ -36,6 +59,19 @@ class PagesController < ApplicationController
 		    ['rgba(201, 107, 69, 1)','rgba(201, 107, 69, 0.6)','rgba(201, 107, 69, 0.3)'],
 		    ['rgba(182, 151, 37, 1)','rgba(182, 151, 37, 0.6)','rgba(182, 151, 37, 0.3)'],
 		    ['rgba(32, 48, 106, 1)','rgba(32, 48, 106, 0.6)','rgba(32, 48, 106, 0.3)']]
+	end
+
+	def show_search
+		@search = params[:search]
+		@results_json = HTTParty.get("https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=#{params[:search]}").parsed_response
+		@colors = [
+			['rgba(53, 92, 125, 1)','rgba(53, 92, 125, 0.6)', 'rgba(53, 92, 125, 0.3)'],
+		    ['rgba(163, 111, 72, 1)','rgba(163, 111, 72, 0.6)','rgba(163, 111, 72, 0.3)'],
+		    ['rgba(201, 107, 69, 1)','rgba(201, 107, 69, 0.6)','rgba(201, 107, 69, 0.3)'],
+		    ['rgba(182, 151, 37, 1)','rgba(182, 151, 37, 0.6)','rgba(182, 151, 37, 0.3)'],
+		    ['rgba(32, 48, 106, 1)','rgba(32, 48, 106, 0.6)','rgba(32, 48, 106, 0.3)']]
+
+
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  get 'pages/show_season/:season_name/:sid', to: 'pages#show_season', as: 'season'
+
   get 'pages/show_episode/:eid', to: 'pages#show_episode', as: 'episode'
 
   get 'pages/show_character/:character_name', to: 'pages#show_character', as: 'character'
 
+  get 'pages/show_search', to: 'pages#show_search', as: 'search'
 
- 
 end
